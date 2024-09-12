713ba321801ebda6713030f5429cbaa5f03c2998
Grootcode21 <antonyndichu15@gmail.com>
2024-09-11 23:45:04 -0700

    default db settings

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Grootcode21',
+        'USER': 'root',
+        'PASSWORD': '@Inferni5',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
